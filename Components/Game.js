57d74b1a0354060a28675dd06533a7bfa7e6c816
gituser <test@test.de>
2023-07-31 11:20:17 +0200

    Rico

--- Components/Game.js
+++ Components/Game.js
@@ -17,4 +17,52 @@ class Game {
         }
       }
 
-}
+
+      checkWinner(maxZeile, maxSpalte, playerID){
+        	//horizontally
+          for(zeile = 0; zeile< maxZeile; zeile++) {
+            for(spalte = 0; spalte < maxSpalte -3; spalte++){
+              if(spielfeld[zeile][spalte] = ' '){
+                if(spielfeld[zeile][spalte] === playerID && spielfeld[zeile][spalte+1] === playerID && spielfeld[zeile][spalte+2] === playerID && spielfeld[zeile][spalte+3] === playerID) {
+                  //setWinner()
+                  return true;
+                }
+            }
+          }
+        }
+          //vertically
+          for(zeile = 0; zeile< maxZeile; zeile++) {
+            for(spalte = 0; spalte < maxSpalte -3; spalte++){
+              if(spielfeld[zeile][spalte] = ' '){
+                if(spielfeld[zeile][spalte] === playerID && spielfeld[zeile][spalte+1] === playerID && spielfeld[zeile][spalte+2] === playerID && spielfeld[zeile][spalte+3] === playerID) {
+                  //setWinner()
+                  return true;
+                }
+            }
+          }
+        }
+          //anti diagonally
+          for(zeile = 0; zeile< maxZeile; zeile++) {
+            for(spalte = 0; spalte < maxSpalte -3; spalte++){
+              if(spielfeld[zeile][spalte] = ' '){
+                if(spielfeld[zeile][spalte] === playerID && spielfeld[zeile][spalte+1] === playerID && spielfeld[zeile][spalte+2] === playerID && spielfeld[zeile][spalte+3] === playerID) {
+                  //setWinner()
+                  return true;
+                }
+            }
+          }
+        }
+          //diagonally
+          for(zeile = 0; zeile< maxZeile; zeile++) {
+            for(spalte = 0; spalte < maxSpalte -3; spalte++){
+              if(spielfeld[zeile][spalte] = ' '){
+                if(spielfeld[zeile][spalte] === playerID && spielfeld[zeile][spalte+1] === playerID && spielfeld[zeile][spalte+2] === playerID && spielfeld[zeile][spalte+3] === playerID) {
+                  //setWinner()
+                  return true;
+                }
+            }
+          }
+        }
+      }
+    
+    }
