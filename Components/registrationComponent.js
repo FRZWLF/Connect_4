59e1e8b0d51e01e30a1a965dfb2ba74503445e5e
gituser <test@test.de>
2023-07-31 16:35:37 +0200

    bugfixes idriss,rene,Marvin,Lukas

--- Components/registrationComponent.js
+++ Components/registrationComponent.js
@@ -1,21 +1,26 @@
 const crypto = require("crypto")
 const User = require("../Model/User")
+const val = require(`validator`)
+
+
 
 class RegistrationComponent {
 
-    constructor(){
-            window.register = this.register.bind(this)
+    constructor() {
+        window.register = this.register.bind(this)
     }
 
     getHTML() {
         var text = /*html*/`
     <h2> Registrierung </h2>
-    Username:<input type="text" id="username"><br>
-    Password:<input type="password" id="password"><br>
-    Password again<input type="password" id="password2"><br>
-    First Name<input type="text" id="firstname"><br>
-    Surname<input type="text" id="surname"><br>
+    Username:<input type="text" id="username" required><br>
+    Password:<input type="password" id="password" required><br>
+    Password again:<input type="password" id="password2" required><br>
+    First Name:<input type="text" id="firstname"><br>
+    Surname:<input type="text" id="surname"><br>
     E-mail:<input type="email" id="email"><br>
+
+    
     <button onclick="register()" >Registrieren</button>
     `
 
@@ -31,30 +36,36 @@ class RegistrationComponent {
         let surname = document.getElementById("surname").value
         let email = document.getElementById("email").value
 
+        if (username == "" || password == "") {
+            alert("Username und Passwort muss angegeben werden")
+        } else {
 
-        if (password == password2) {
-            var hash = crypto.createHash('sha256')
-            hash.update(password)
-            let pwHash = hash.digest('hex')
+            if (password == password2 && val.isEmail(email)) {
+                var hash = crypto.createHash('sha256')
+                hash.update(password)
+                let pwHash = hash.digest('hex')
 
-            const user = new User(username, pwHash, firstname, surname, email)
+                const user = new User(username, pwHash, firstname, surname, email)
 
-            socket.emit("registration", user)
+                socket.emit("registration", user)
 
-            socket.on("regisanswer", (answer) => {
-                if (answer) {
-                    alert("Registration fehlgeschlagen")
-                } else {
-                    alert("User wurde angelegt")
-                }
-            })
+                socket.on("regisanswer", (answer) => {
+                    if (answer) {
+                        alert("Registration fehlgeschlagen")
+                    } else {
+                        alert("User wurde angelegt")
+                    }
+                })
 
 
+
+            }
+            else {
+                alert("Passwörter sind ungleich oder die Email ist ungültig!")
+            }
 
         }
-        else{
-            alert("Passwörter sind ungleich!")
-        }
+
 
     }
 }
